5199dfcffd7524709ceeeeb4e31efcf6e27e87d8
RammeDon <raminkhareji@gmail.com>
2023-12-26 15:24:11 +0100

    Setting for the build folder to be added into the server directory and showing on the same domain

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const planetRouter = require("./routes/planets/planets.router");
 
@@ -10,6 +11,15 @@ app.use(
   })
 );
 app.use(express.json());
+
+// This is to show the react app
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// This is to make sure that when the domain alone is called it defaults to the index.html file
+// In the production build
+app.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 app.use(planetRouter);
 
