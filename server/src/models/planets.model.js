2db173554d7d44471ea23d4ada77281deec82ace
RammeDon <raminkhareji@gmail.com>
2023-12-30 12:31:22 +0100

    Working on adding function to create a planet for all inhabitable planets in the CSV file but not duplicating

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,6 +2,8 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 const path = require("path");
 
+const planets = require("./planets.mongo");
+
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
@@ -24,9 +26,13 @@ function loadPlanets() {
           columns: true,
         })
       )
-      .on("data", (data) => {
+      .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          // habitablePlanets.push(data);
+          // TODO: Replace Create with upsert (insert + update)
+          // await planets.create({
+          //   keplerName: data.kepler_name,
+          // });
         }
       })
       .on("error", (err) => {
