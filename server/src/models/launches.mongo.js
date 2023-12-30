af9c01df7c6cd6ceeda5095665d6a9a3f4300d2b
RammeDon <raminkhareji@gmail.com>
2023-12-30 12:09:31 +0100

    adding schema for launches

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const launchesSchema = new mongoose.Schema({
+  flightnumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: [string],
+  upcoming: {
+    type: boolean,
+    required: true,
+  },
+  success: {
+    type: boolean,
+    required: true,
+    default: true,
+  },
+});
