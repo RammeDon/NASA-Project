0142afcefe599da1e9ef9782db127e7142a2a00a
RammeDon <raminkhareji@gmail.com>
2023-12-30 12:25:55 +0100

    Small clean up

--- server/src/models/planets.mongo.js
+++ server/src/models/planets.mongo.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
-const planetsSchema = new mongoose.Schema({
+const planetSchema = new mongoose.Schema({
   keplerName: {
     type: "string",
     required: true,
   },
 });
+
+module.exports = mongoose.model("Planet", planetSchema);
